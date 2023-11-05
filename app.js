6ed0e84d6592185b4389de18ae906ebec6500b5a
SAHIL080603 <sahil14874@gmail.com>
2023-11-05 12:05:20 +0530

    Arithmatic-v2

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const express=require('express');
 const app=express();
+const port = process.env.PORT || 3000;
 
 function Arithmatic_String(operands,minimum,maximum,decimalPlaces){
     let a=operands;
@@ -66,6 +70,6 @@ app.get('/sample',(req,res)=>{
 })
 
 
-app.listen(4000,()=>{
+app.listen(port,()=>{
     console.log('Server started listening at port 4000');
 })
